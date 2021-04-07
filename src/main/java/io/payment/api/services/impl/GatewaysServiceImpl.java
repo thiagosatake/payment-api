374ef2d2aa7d0c35dffe361ef91446ac19de6ed9
Thiago Seito Satake <thiago.seito.satake@gmail.com>
2021-04-06 23:28:59 -0300

    Clean up

--- src/main/java/io/payment/api/services/impl/GatewaysServiceImpl.java
+++ src/main/java/io/payment/api/services/impl/GatewaysServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.postgresql.util.PSQLException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
